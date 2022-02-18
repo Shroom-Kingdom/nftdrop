4a0181e3976c7a483174319a2a79c628e2198641
Tarnadas <Tarnadas@users.noreply.github.com>
2022-02-18 15:09:08 +0000

    deploy: 0ac5973d3dd441b56dbe61ec612529af65a0079a

---
+++ _next/static/nZYp3AUtQvnaWtp_02nQ1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/529-a535a6b8e66718c85dd3.js","static/chunks/pages/index-b8915a60184994e195e5.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
