88f6f7fc44dcf877ec1994842d9d22d415a08402
Tarnadas <Tarnadas@users.noreply.github.com>
2022-03-13 20:10:43 +0000

    deploy: 06236b7eeb2f98668755bd9658bf4609e50ef13e

--- _next/static/OhwECoxuv9KhCS1754DbZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/202-e333baba4ec6de9aba86.js","static/chunks/pages/index-a3f3379c42edd8249483.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
